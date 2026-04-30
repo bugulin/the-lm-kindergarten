1c6cba7bf54d241b19d1e946b35c0398ce7bd3bb
Jajasek <jachym.mierva@gmail.com>
2026-04-30 16:01:50 +0200

    Add logging to training_job.sh

--- scripts/training_job.sh
+++ scripts/training_job.sh
@@ -3,6 +3,14 @@
 #PBS -l select=1:ncpus=2:ngpus=1:mem=16gb:gpu_mem=24gb:scratch_local=64gb
 #PBS -l walltime=15:00:00
 
+# Print out the basic info about the job
+echo "Hello ${PBS_JOBNAME} at $(date) from user ${USER}!"
+echo "${PBS_JOBID} is running on node $(hostname -f) in a scratch directory ${SCRATCHDIR}"
+echo "Command: $0 ${BRANCH} ${FINE_TUNE_ARGS[*]}"
+
+set -x
+set -e
+
 usage() {
     echo "Usage: $0 [--branch BRANCH] [FINE_TUNE_ARGS...]"
     echo
@@ -52,11 +60,6 @@ if [[ -f "${hf_token_file}" ]]; then
     HF_TOKEN=$(cat "${hf_token_file}")
     export HF_TOKEN
 fi
-
-# Print out the basic info about the job
-echo "Hello ${PBS_JOBNAME} at $(date) from user ${USER}!"
-echo "${PBS_JOBID} is running on node $(hostname -f) in a scratch directory ${SCRATCHDIR}"
-echo "Command: $0 ${BRANCH} ${FINE_TUNE_ARGS[*]}"
 
 # Run the main task
 bin/uv run src/cli.py fine-tune -o "${outdir}" "${FINE_TUNE_ARGS[@]}"
